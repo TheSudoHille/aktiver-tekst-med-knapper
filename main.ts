1269a918c6f82677f3f86b3b433db1f95c8be9a8
TheSudoHille <h.theander@icloud.com>
2025-03-02 11:16:59 +0100

    De første filer til MakeCode-projekt

---
+++ main.ts
@@ -0,0 +1,9 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("Han Solo")
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showString("Star Wars Rules!")
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showString("Yoda")
+})
